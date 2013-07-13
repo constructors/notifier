4990a00e2652e27ac27eefe5813ad14eccf1b252
Joris van Rooij <jorrizza@jrrzz.net>
2013-07-13 17:21:36 +0200

    First working version.

---
+++ daemon.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler/setup'
+require 'xmpp4r'
+require 'xmpp4r/muc/helper/simplemucclient'
+require 'mkfifo'
+require 'daemons'
+require File.expand_path('config.rb', File.dirname(__FILE__))
+
+File.unlink(FIFO) rescue nil
+begin
+  File.mkfifo(FIFO)
+  File.chmod(0666, FIFO)
+rescue
+  $stderr.puts $!
+  exit 1
+end
+
+Daemons.run_proc("notifier-daemon") do
+  client = Jabber::Client.new(JID)
+  client.connect
+  client.auth(PASS)
+  muc = Jabber::MUC::SimpleMUCClient.new(client)
+  muc.join(ROOM + '/hekker')
+
+  loop do
+    fifo = File.open(FIFO)
+    while str = fifo.gets
+      muc.send Jabber::Message.new(muc.room, str.strip)
+    end
+    fifo.close
+  end
+end
